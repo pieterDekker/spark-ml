5a6dde4a3553958fe9ae68b6195a791f20a8d0d3
pieterDekker <pieter.dekker20@gmail.com>
2024-02-15 00:41:45 +0100

    use EmptyOperator instead of deprecated DummyOperator

--- airflow/dags/hello-airflow-spark.py
+++ airflow/dags/hello-airflow-spark.py
@@ -1,15 +1,13 @@
 from airflow.models.dag import DAG
 
-from airflow.operators.dummy_operator import DummyOperator
-
-from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 with DAG(
     "hello-airflow-spark",
 ) as dag:
-    start = DummyOperator(task_id="start", dag=dag)
+    start = EmptyOperator(task_id="start", dag=dag)
     submit_job = SparkSubmitOperator(
         task_id="submit_job",
         application="/opt/spark-apps/hello-spark.py",
@@ -17,5 +15,5 @@ with DAG(
         verbose=True,
         dag=dag,
     )
-    end = DummyOperator(task_id="end", dag=dag)
+    end = EmptyOperator(task_id="end", dag=dag)
     start >> submit_job >> end
